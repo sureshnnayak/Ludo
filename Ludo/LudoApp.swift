83e95b1da2daaa9a31ef6943f599dacc245de399
Suresh Nayak <sureshnnayak24@gmail.com>
2022-11-11 18:07:57 -0700

    Initial Commit

---
+++ Ludo/LudoApp.swift
@@ -0,0 +1,17 @@
+//
+//  LudoApp.swift
+//  Ludo
+//
+//  Created by Suresh Narasimha Nayak on 11/11/22.
+//
+
+import SwiftUI
+
+@main
+struct LudoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
